81e717a56f4476058a19b98011db0ae9c55b1dc0
Sachin Kumar Sah <sachinhangupcrew@gmail.com>
2025-12-04 03:51:26 +0530

    sybarrays of array

---
+++ Arrays/subarrays_of_an_array.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Scanner;
+
+public class subarrays_of_an_array {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the array n");
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i; j < arr.length; j++){
+                for(int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " \t");
+                }
+                System.out.println();
+            }
+        }
+        
+        
+
+        
+    }
+}
