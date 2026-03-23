58d32ccadb64394e8b478d819e968d46169c8fe6
Sachin Kumar Sah <sachinhangupcrew@gmail.com>
2026-03-23 20:12:01 +0530

    tower of hanoi

---
+++ Recursion/test.java
@@ -0,0 +1,10 @@
+
+//ham Scanner use kia bina command line se input deke output nikal skte hai
+
+public class test {
+    public static void main(String[] args){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+    
+}
